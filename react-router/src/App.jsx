11adaf0f948a28072918cba23010a6e026fbee1f
AndelsonOliveira1995 <JOliveiraC1995@gmail.com>
2024-09-25 08:47:14 -0300

    Atualização

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,28 +1,59 @@
-import './App.css'
-import { imagens } from './assets/imagens';
-import NavBar from './Componentes/NavBar'
-import {useEffect} from 'react'
-import 'materialize-css/dist/css/materialize.min.css'
-import 'materialize-css/dist/js/materialize.min'
+import "./App.css";
+// import { imagens } from './assets/imagens';
+import NavBar from "./Componentes/NavBar";
+import { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min";
+import { imagens } from "./assets/imagens";
 
 function App() {
-
   useEffect(() => {
-    
     const M = window.M; // Certifique-se de que Materialize está disponível no escopo global
-    const instance  = document.querySelectorAll('.carousel .carousel-slider');
-    M.Carousel.init({instance, fullWidth: true}); 
+    const instance = document.querySelectorAll(".carousel .carousel-slider");
+    M.Carousel.init({ instance, fullWidth: true });
   }, []);
 
   return (
     <>
       <NavBar />
-      <div className="carousel carousel-slider">
-        <a className="carousel-item"><img src={imagens.slide01} /></a>
-        <a className="carousel-item"><img src={imagens.slide02} /></a>
+      <div className="container">
+        <div className="row">
+          <div className="col s12">
+            <article className="col s12 m3 l4">
+              <div className="card">
+                <div className="card-image">
+                  <img src={imagens.slide02} alt="" />
+                </div>
+                <div className="card-content">
+                  <p>Primeiro artigo</p>
+                </div>
+              </div>
+            </article>
+            <article className="col s12 m3 l4">
+              <div className="card">
+                <div className="card-image">
+                  <img src={imagens.slide02} alt="" />
+                </div>
+                <div className="card-content">
+                  <p>Primeiro artigo</p>
+                </div>
+              </div>
+            </article>
+            <article className="col s12 m3 l4">
+              <div className="card">
+                <div className="card-image">
+                  <img src={imagens.slide02} alt="" />
+                </div>
+                <div className="card-content">
+                  <p>Primeiro artigo</p>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
